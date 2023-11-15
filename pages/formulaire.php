4ed70c2bdacd56aa3399aeeebaf4e165e0712bcb
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-11-15 14:26:59 -0500

    création formulaire

--- pages/formulaire.php
+++ pages/formulaire.php
@@ -11,37 +11,39 @@
         <div1 class="name-field">
         <?php
              // Formulaire permettant d'enregistrer un nouvel utilisateur en fonction des adresses entrées
-        $nbAdresses = $_POST['nb_adresses'];
-        for ($i = 1; $i <= $nbAdresses; $i++) {
+        $address = $_POST['address'];
+        for ($i = 1; $i <= $address; $i++) {
             echo "<br><br><h3>Address $i :</h3>
         <div>
         <label for='street'>Street:</label> 
-        <input type='text' name='street $i' id='street' maxlength='50' required /><br><br> </div>
+        <input type='text' name='street $i' id='street' maxlength='50' placeholder='Veuillez entrer votre street' required /><br><br> </div>
         <div>
         <label for='street_nb'>Street Number:</label> 
-        <input type='number' name='street_nb $i' id='street_nb' required /><br><br> </div>
+        <input type='number' name='street_nb $i' id='street_nb' placeholder='Veuillez entrer votre street_nb'required /><br><br> </div>
         <div>
         <label for='type'>Type:</label>
-        <select name='type $i' id='type ' required >
+        <select name='type $i' id='type 'placeholder=' veuillez selectionner votre type' required >
             <option value='livraison'>Livraison</option>
             <option value='facturation'>Facturation</option>
             <option value='autre'>Autre</option>
         </select><br><br> </div>
         <div>
-        <label for='city'>City:</label>
-        <select name='city $i' id='city ' required >
+        <div1>
+        <label for='city'>City:</label><div1>
+        <select name='city $i' id='city  placeholder='veuillez selectionner votre city'' required >
             <option value='Montréal'>Montréal</option>
             <option value='Laval'>Laval</option>
             <option value='Toronto'>Toronto</option>
             <option value='Québec'>Québec</option><br><br>
         </select><br><br>
         </div>
-        <div>
-        <label for='zipcode'>Zipcode:</label>
+        <div><div1>
+        <label for='zipcode'>Zipcode:</label></div1>
         <input
             type='text'
             name='zipcode $i'
-            id='zipcode'    
+            id='zipcode'  
+            placeholder='Veuillez entrer votre zipcode '  
         />
         <br><br> </div>
         ";
