05b6b67497a821d16f29ef8ac9c7e41b49423d01
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-11-16 12:25:28 -0500

    Confirmation de données

--- pages/Confirmation.php
+++ pages/Confirmation.php
@@ -1,6 +1,3 @@
-<!-- <?php
-require_once "../configuration/connexion.php";
-?>
-<h2> Veuillez confirmer vos informations  sur vos addresses </h2>
 <?php
-?> -->
+require_once "../configuration/connexion.php";
+var_dump($_POST);
