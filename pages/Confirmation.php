1ea2e8ca8db811870f1b6c9f01546ff6999c2c4a
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-11-16 15:03:06 -0500

    confirmatioons des informations

--- pages/Confirmation.php
+++ pages/Confirmation.php
@@ -1,3 +1,12 @@
-<?php
-require_once "../configuration/connexion.php";
-var_dump($_POST);
+ <!-- <?php
+//  require_once("../functions/crud.php");
+//  if (isset($_POST)){
+//     $nb_adresses=(int)$_POST['valeur'];
+//     for($i = 1; $i <= $nb_adresses; $i++)
+//     $data=[
+//         "street"=>$_POST["street $i"],
+//         "street_nb"=>$_POST["street_nb $i"],
+//     ];
+//     var_dump($data);
+//  }
+ ?> -->
