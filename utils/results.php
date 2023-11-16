68a0482d339d3b2f0eb5700f4c2ea7be1fa0bfef
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-11-16 11:47:44 -0500

    Resultat de formulaire

--- utils/results.php
+++ utils/results.php
@@ -2,5 +2,20 @@
 require_once "../configuration/connexion.php";
 require_once "../functions/crud.php";
 require_once "../functions/validation.php";
-var_dump($_POST);
+//var_dump($_POST);
 ?>
+<h2>Veuillez confirmer vos informations</h2>
+<div>
+<?php
+var_dump($_POST);
+$infos = $_POST;
+echo "<form action=''>";
+if(count($infos) > 0) {
+    foreach($infos as $key => $value) {
+        echo "<input type='text' id='$key' value='$value' readonly /> <br />";
+    }
+}
+echo "<button type='submit'>Confirmer</button>";
+echo "</form>";
+?>
+</div>
