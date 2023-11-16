ba4a390165cfd34538cc736f2f87f17b65d56473
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-11-16 17:16:33 -0500

    création des résultats

--- utils/results.php
+++ utils/results.php
@@ -2,25 +2,25 @@
 require_once "../configuration/connexion.php";
 require_once "../functions/crud.php";
 require_once "../functions/validation.php";
-//var_dump($_POST);
 ?>
 <h2>Veuillez confirmer vos informations</h2>
 <div>
 <?php
 var_dump($_POST);
-$data = $_POST;
-echo "<form action='../pages/confirmation.php' method='post'>";
-if(count($data) > 0) {
-    foreach($data as $key => $value) {
+$infos = $_POST;
+echo "<form action='../pages/essaie.php' method='post'>";
+if(count($infos) > 0) {
+    foreach($infos as $key => $value) {
         echo "<input type='text' name='$key' value='$value' readonly /> <br />";
     }
 }
 echo "<br/> <br />";
 echo "<button type='submit'>Confirmer</button>";
-echo "<br/> <br />";
+echo "<br/>";
 echo "<button type='reset'>Effacer</button>";
 echo "<br/> <br />";
-echo "<a href = ../pages/formulaire.php'>Retour</a>";
-echo "</form>";
 ?>
+<!-- <a href ="../pages/formulaire.php">Retour</a>; -->
+</form>;
+
 </div>
