9f4930dc29607d05d734e190185b9a6d6dd98611
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-11-17 10:52:04 -0500

    connexion vers la base de données

---
+++ utils/addToDB.php
@@ -0,0 +1,17 @@
+<?php
+require_once('../functions/crud.php');
+session_start();
+var_dump($_SESSION["formData"]);
+$newData=$_SESSION["formData"];
+for ($i = 1; $i <= $_SESSION['addressnb']; $i++){
+    $newAddressData = [
+        "street" => $_SESSION["formData"]["street$i"],
+        "street_nb" => $_SESSION["formData"]["street_nb$i"],
+        "type" => $_SESSION["formData"]["type$i"],
+        "city"=> $_SESSION["formData"]["city$i"],
+        "zipcode"=> $_SESSION["formData"]["zipcode$i"],
+    ];
+    //ajouter ladresse dans la base donnees
+    createAddress($newAddressData);
+}
+?>
