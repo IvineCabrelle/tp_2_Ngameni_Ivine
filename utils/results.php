5931ac921d1c119981c3baab9844cd09edbd9af8
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-11-18 20:30:42 -0500

    resultats de données

--- utils/results.php
+++ utils/results.php
@@ -2,55 +2,57 @@
 require_once "../configuration/connexion.php";
 require_once "../functions/crud.php";
 require_once "../functions/validation.php";
-//La page résults va permettre de faire le traitement de tous les formulaires  
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Confirmations des informations</title>
+    <title>Confirmation</title>
 </head>
 <body>
+    
+</body>
+</html>
 <h2>Veuillez confirmer vos informations</h2>
-<head>
-<link rel="stylesheet" type="text/css" href="../style css/style3.css" /></head>
-<body>
 <div>
+<link rel="stylesheet" type="text/css" href="../style css/style3.css" />
 <?php
-echo "<b><center> Le nombre d'adresse que vous avez saisi est :</br>";
-echo $_SESSION['addressnb'];
 ?>
 <form method='post' action='addToDB.php'>
 <?php
+echo "<b> Le nombre d'adresse choisis est de :<b> ";
+echo $_SESSION['addressnb'];
+
 for ($i = 1; $i <= $_SESSION['addressnb']; $i++){
     $_SESSION["formData"]=$_POST;
+    
     $streetIsValid=streetIsValid($_POST["street$i"]);
     $zipCodeIsValid =zipCodeIsValid($_POST["zipcode$i"]);
 }
 $infos = $_POST;
 echo "<form action='addToDB.php' method='post'>";
+
 if ($streetIsValid["isValid"] &&$zipCodeIsValid["isValid"]){
 if(count($infos) > 0) {
     foreach($infos as $key => $value) {
         echo "<input type='text' name='$key' value='$value' readonly /> <br />";
     }
 }
-}else {
-    echo "<b> erreur de connexion</b>";
-    //redirige vers la page du formulaire
-    header('Location: ../pages/formulaire.php');
-    exit;}
-        
+}else{
+    echo "erreur de connection ";
+    
+}
+
 echo "<br/> <br />";
-echo "<button type='submit'>Confirmer</button>";
+echo "<button type='submit'>Confirmer</button></a>";
 echo "<br/>";
+echo "<br/> <br />";
 
 ?>
-</div>
-</form>;
-</body>
-</html>
 
+</form>;
+
+</div>
 
 
