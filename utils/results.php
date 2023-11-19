6fd6cd47e33be68fee10de297f565e74efe93cba
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-11-18 19:33:41 -0500

    resultats de données

--- utils/results.php
+++ utils/results.php
@@ -2,16 +2,27 @@
 require_once "../configuration/connexion.php";
 require_once "../functions/crud.php";
 require_once "../functions/validation.php";
+//La page résults va permettre de faire le traitement de tous les formulaires  
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirmations des informations</title>
+</head>
+<body>
 <h2>Veuillez confirmer vos informations</h2>
 <head>
-<link rel="stylesheet" type="text/css" href="../style css/style3.css" />
-</head>
+<link rel="stylesheet" type="text/css" href="../style css/style3.css" /></head>
+<body>
 <div>
 <?php
 echo "<b><center> Le nombre d'adresse que vous avez saisi est :</br>";
 echo $_SESSION['addressnb'];
-
+?>
+<form method='post' action='addToDB.php'>
+<?php
 for ($i = 1; $i <= $_SESSION['addressnb']; $i++){
     $_SESSION["formData"]=$_POST;
     $streetIsValid=streetIsValid($_POST["street$i"]);
@@ -26,19 +37,20 @@ if(count($infos) > 0) {
     }
 }
 }else {
-    echo "error de connexion";
-    //redirect vers formulaire page
+    echo "<b> erreur de connexion</b>";
+    //redirige vers la page du formulaire
     header('Location: ../pages/formulaire.php');
     exit;}
         
 echo "<br/> <br />";
-echo "<a href ='addToDB.php'><button type='submit'>Confirmer</button></a>";
+echo "<button type='submit'>Confirmer</button>";
 echo "<br/>";
 
 ?>
-</head>
-</form>;
-
 </div>
+</form>;
+</body>
+</html>
+
 
 
